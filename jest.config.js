33615315457ef10a981dcf76967645feccc54e6c
Andres Loachamin <67283207+BochaXD@users.noreply.github.com>
2025-06-03 23:18:13 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "text", "json-summary"],
+
   reporters: [
     "default",
     [
@@ -12,5 +16,14 @@ module.exports = {
         ancestorSeparator: " → "
       }
     ]
-  ]
+  ],
+
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
