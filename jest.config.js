85ac5a3837f1e6a30a381f42616787d62222fb6d
Andres Loachamin <67283207+BochaXD@users.noreply.github.com>
2025-06-04 15:31:09 -0500

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "text", "json-summary"],
+
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "reports",
+        outputName: "junit.xml",
+        suiteName: "NodeJS Test Report",
+        classNameTemplate: "{classname} → {title}",
+        titleTemplate: "{title}",
+        ancestorSeparator: " → "
+      }
+    ]
+  ],
+
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+};
