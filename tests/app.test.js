f6d812f1dbb0dfed3a81daf70f4bccc2677447aa
Andres Loachamin <67283207+BochaXD@users.noreply.github.com>
2025-06-03 23:10:22 -0500

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -20,19 +20,11 @@ describe("API Endpoints", () => {
     expect(res.statusCode).toBe(400);
     expect(res.body.error).toBe("Invalid input");
   });
-/*     test('Falla/Error: Simulación de error', () => {
-    expect(() => {
-      throw new Error('Esto es un error simulado');
-    }).toThrow('Otro mensaje'); // ❌ falla porque no coincide
-  }); */
 
   test.skip('Omitido: esta prueba fue omitida', () => {
     expect(true).toBe(false); // 🟠 omitido
   });
 
-  test('Desconocido: falla asincrónica sin assert', async () => {
-    await new Promise((_, reject) => setTimeout(() => reject('Desconocido'), 10));
-  });
   function evaluar(valor) {
   if (valor > 0) return "positivo";
   else if (valor < 0) return "negativo";
